f8edf4c36348d1ced8dda1acf5a7314a6a338694
pariramb <yana.pigunova.04@bk.ru>
2025-07-22 07:49:32 +0300

    chunking module with llama-index

---
+++ text-preprocessing/chunk_getter.py
@@ -0,0 +1,87 @@
+from llama_index.core import SimpleDirectoryReader
+import json
+import argparse
+from llama_index.core.node_parser import (
+    SimpleNodeParser,
+    SentenceSplitter
+)
+from yandex_cloud_ml_sdk import YCloudML
+from llama_index.core import Document, Settings
+
+class Data:
+    def __init__(self,basic_promt: str,
+                 data_path: str,
+                 format:str = "text",
+                 chunker:str = "basic",
+                 chunk_size = 150,
+                 chunk_overlap = 50):
+        self.basic_promt = basic_promt
+        self.data_path = data_path
+        self.format = format
+        self.chunker = chunker
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        self.sdk = YCloudML(
+          folder_id="",
+          auth="",
+        )
+
+    def basic_chunks(self, docs: list[str]):
+        base_splitter = SimpleNodeParser.from_defaults(chunk_size = self.chunk_size,
+                                                       chunk_overlap = self.chunk_overlap)
+        nodes = base_splitter.get_nodes_from_documents(docs)
+        return nodes
+
+    def llm_chunks(self, docs: list[str]):
+        new_docs = []
+
+        for doc in docs:
+            messages = [
+                {
+                    "role": "user",
+                    "text": "Rewrite the text so that each sentence contains exactly one fact. Leave the sequence of events unchanged",
+                },
+                {
+                    "role": "user",
+                    "text": str(doc),
+                }
+            ]
+            new_chunks = (
+                self.sdk.models.completions("yandexgpt-lite").configure(temperature=0.5).run(messages)
+            )
+            
+            assert len(new_chunks.text) != 0, "no result"
+
+            new_doc = new_chunks.text
+            sents = new_doc.split(".")
+            new_docs.extend([sent for sent in sents if sent.strip()])
+
+        docs = [Document(text=doc) for doc in new_docs]
+        parser = SentenceSplitter()
+        return parser.get_nodes_from_documents(docs)
+            
+
+def main():
+    parser = argparse.ArgumentParser(description="Collision detection.")
+    parser.add_argument("--input_json", type=str, help="Path to input json")
+
+    args = parser.parse_args()
+
+    with open(args.input_json, "r") as file:
+        input_json = json.load(file)
+        data = Data(input_json["conflict"], input_json["data_path"], chunker="LLM")
+
+    docs = []
+    if data.format == "text":
+        docs = SimpleDirectoryReader(input_dir=data.data_path, recursive=True).load_data()
+    if data.format == "csv":
+        pass
+
+    nodes = []
+    if data.chunker == "basic":
+        nodes = data.basic_chunks(docs)
+    if data.chunker == "LLM":
+        nodes = data.llm_chunks(docs)
+
+if __name__ == '__main__':
+    main()
